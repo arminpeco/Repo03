ea61b80d34719dffefc0c624243a2be5ed26fba6
arminpeco <apeco1906@gmail.com>
2021-09-15 15:41:46 +0200

    Added file index

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this line
+    
+        // five letters
+        /* five letters */
+    //Only change code below this line
+
+
+}
+main();
+
+module.exports=main;
